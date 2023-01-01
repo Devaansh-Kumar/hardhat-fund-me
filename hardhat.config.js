3b304d819116abd4f04c17acdbc14303df0cbdcf
Devaansh Kumar <devaansh840@LAPTOP-1GJ1OTPU.localdomain>
2023-01-01 14:45:15 +0530

    commit

---
+++ hardhat.config.js
@@ -0,0 +1,52 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+require("solidity-coverage");
+require("hardhat-deploy");
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+
+module.exports = {
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+        url: GOERLI_RPC_URL,
+        accounts: [PRIVATE_KEY],
+        chainId: 5,
+        blockConfirmations: 6,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gasReporter.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "ETH",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    user: {
+      default: 1,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+    // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
+
+},
+};
---
+++ test/staging/FundMe.staging.test.js
@@ -0,0 +1,22 @@
+const { assert, expect } = require("chai");
+const { deployments, ethers, getNamedAccounts, network } = require("hardhat");
+const { developmentChains } = require("../../helper-hardhat-config");
+
+developmentChains.includes(network.name)
+  ? describe.skip
+  : describe("FundMe", async () => {
+      let fundMe;
+      let deployer;
+      const sendValue = ethers.utils.parseEther("0.09");
+      beforeEach(async () => {
+        deployer = (await getNamedAccounts()).deployer;
+        fundMe = await ethers.getContract("FundMe", deployer);
+      });
+
+      it("allows people to fund and withdraw from a contract", async () => {
+        await fundMe.fund({value: sendValue});
+        await fundMe.withdraw();
+        const endingBalance = await fundMe.provider.getBalance(fundMe.address);
+        assert.equal(endingBalance.toString(), "0");
+      })
+    });
